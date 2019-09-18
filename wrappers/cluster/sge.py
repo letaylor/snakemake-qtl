49aba03da261f690c6934af5b048c0f59cf6fbc8
Leland Taylor <letaylor@users.noreply.github.com>
2019-09-18 15:32:15 -0400

    added sge config

--- wrappers/cluster/sge.py
+++ wrappers/cluster/sge.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# submits lsf jobs
+# submits sge jobs
 
 import os
 import sys
@@ -15,9 +15,6 @@ job_properties = read_job_properties(jobscript)
 
 cluster = job_properties.get('cluster', {})
 
-# pass customized flags (e.g., -P bigmem)
-custom_flags = cluster.get('custom_flags', None)
-
 group = cluster.get('group', 'snakemake') # job group
 queue = cluster.get('queue', None) # job queue
 job_name = cluster.get('name', 'snakemake') # job name
@@ -28,7 +25,7 @@ if memory:
     # mem is specified as total memory in Gb, but we
     # need to give it to SGE as memory per thread in Mb
     memory = int(math.floor(float(memory * 1e9) / (threads * 1e6)))
-resources = 'select[mem>{}] rusage[mem={}] span[hosts=1]'.format(
+resources = 'mem_free={0}M,h_vmem={0}M'.format(
     memory, memory
 )
 
@@ -43,29 +40,24 @@ if error:
     tdir = os.path.dirname(error)
     if not os.path.exists(tdir): os.makedirs(tdir)
 
+
 # build the command
-cmd = 'bsub'
-if custom_flags:
-    cmd = '{} {}'.format(cmd, custom_flags)
+cmd = 'qsub'
 if queue: 
     cmd = '{} -q {}'.format(cmd, queue)
-cmd = '{} -g {} -M {} -R "{}" -J "{}"'.format(
+cmd = '{} -N "{}" -V -cwd -l "{}"'.format(
     cmd,
-    group,
-    memory,
-    resources,
-    job_name)
+    job_name,
+    resources
+)
 if threads > 1:
-    # -n n_procs: The expected resource consumption of the task.
-    # -R "select[nprocs>2]": Select hosts with > 2 REAL PROCESSORS (chips).
-    cmd = '{} -E "export OMP_NUM_THREADS={}"'.format(cmd, threads)
-    cmd = '{} -n {}'.format(cmd, threads)
-    # an alternative is to do '-R "affinity[core({})]"'.format(threads)
+    cmd = '{} -pe make-dedicated {}'.format(cmd, threads)
 if output: 
-    cmd = '{} -oo {}'.format(cmd, output)
+    cmd = '{} -o {}'.format(cmd, output)
 if error: 
-    cmd = '{} -eo {}'.format(cmd, error)
+    cmd = '{} -e {}'.format(cmd, error)
 cmd = '{} {}'.format(cmd, jobscript)
+
 
 # run the command
 os.system(cmd)
